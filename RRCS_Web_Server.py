c7afaf267de633eddaef66a231d5d2b9dcfc80ba
Ben Costerton <bencosterton@gmail.com>
2023-04-25 16:45:10 +0100

    Update RRCS_Web_Server.py

--- RRCS_Web_Server.py
+++ RRCS_Web_Server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import os
 import requests
+import socket
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 
@@ -8,12 +10,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# This is the .xml file upload thing
 @app.route('/process_xml', methods=['POST'])
 def process_xml():
     # Get the absolute path of the directory containing this script
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    # Construct the absolute path of the XML file
-    xml_file = os.path.join(script_dir, 'xml', 'TX3_G1.xml')
     
     # Set the server address and port number
     server_address = 'http://127.0.0.1:8193'
@@ -32,6 +33,32 @@ def process_xml():
 
     # Process the response as necessary
     return render_template('results.html', response_text=response.text)
+
+# This is the 'Get IFB List' Button
+@app.route('/button001', methods=['POST', 'GET'])
+def button001_clicked():
+    # Get the absolute path of the directory containing this script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Construct the absolute path of the XML file
+    xml_file = os.path.join(script_dir, 'xml', 'GetIFBList.xml')
+
+    # Set the server address and port number
+    server_address = 'http://127.0.0.1:8193'
+
+    # Read the XML data from file
+    with open(xml_file, 'r') as f:
+        xml_data = f.read()
+    
+    # Set the HTTP headers
+    headers = {'Content-Type': 'text/xml'}
+
+    # Send the HTTP POST request to the server
+    response = requests.post(server_address, data=xml_data, headers=headers)
+
+    # Process the response as necessary
+    return render_template('results.html', response_text=response.text)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5193)
